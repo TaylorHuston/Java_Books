980e5eb8caa3eb039ab305e095006b9e37e105b7
Taylor Huston <taylor.a.huston@gmail.com>
2014-12-07 17:02:18 -0700

    Add description

--- Books.java
+++ Books.java
@@ -1,10 +1,8 @@
 
 import java.util.EnumSet;
-
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * List of books via enum
+ * From "Java How To Program, 10/e, Early Objects" - Chapter 8.
  */
 
 public class Books {
