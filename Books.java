bc73d828c00f7915c597ac45b03003f249107fce
Taylor Huston <taylor.a.huston@gmail.com>
2014-12-30 19:06:13 -0900

    Fix formatting based on checkstyle

--- Books.java
+++ Books.java
@@ -12,9 +12,9 @@ public class Books {
         JHTP("Java How to Program", "2015"),
         CHTP("C How to Program", "2013"),
         IW3HTP("Internet & World Wide Web How to Program", "2012"),
-        CPPHTP("C++ How to Program","2014"),
-        VBHTP("Visual Basic How to Program","2014"),
-        CHSARPHTP("Visual C# how to Program","2014");
+        CPPHTP("C++ How to Program", "2014"),
+        VBHTP("Visual Basic How to Program", "2014"),
+        CHSARPHTP("Visual C# how to Program", "2014");
         
         //Fields
         private final String title;
@@ -24,18 +24,18 @@ public class Books {
         Book(String nTitle, String nYear) {
             this.title = nTitle;
             this.year = nYear;
-        }
+        } //End Constructor
         
         //Getters
         public String getTitle() {
             return title;
-        }
+        } //End getTitle
         
         public String getYear() {
             return year;
-        }
+        } //End getYear
         
-    }//End Book
+    } //End Book
 
     
     //Test for Books
@@ -43,14 +43,20 @@ public class Books {
         //All books
         System.out.println("All books:");
         for (Book book : Book.values()) {
-            System.out.printf("%-10s%-45s%s%n",book, book.getTitle(), book.getYear());
+            System.out.printf("%-10s%-45s%s%n", book, book.getTitle(), 
+                    book.getYear());
         }
         
         //First four books
-        System.out.println("First four books");
+        System.out.println("First four books:");
         for (Book book : EnumSet.range(Book.JHTP, Book.CPPHTP)) {
-            System.out.printf("%-10s%-45s%s%n",book, book.getTitle(), book.getYear());
+            System.out.printf("%-10s%-45s%s%n", book, book.getTitle(), 
+                    book.getYear());
         }
-    }
+        
+        System.out.println("Single book");
+        System.out.printf("%-10s%-45s%s%n", Book.JHTP, Book.JHTP.getTitle(), 
+                    Book.JHTP.getYear());
+    } //End main
     
-}
+} //End Books
